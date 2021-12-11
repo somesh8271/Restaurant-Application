2d6209d293a89cb0961a8afe14ec370449fdd281
somesh8271 <95874959+somesh8271@users.noreply.github.com>
2021-12-11 17:43:08 +0530

    Add files via upload

---
+++ src/redux/comments.js
@@ -0,0 +1,16 @@
+import * as ActionTypes from './ActionTypes';
+
+export const Comments = (state = { errMess: null, comments:[]}, action) => {
+  switch (action.type) {
+      
+    case ActionTypes.COMMENTS_FAILED:
+      return {...state, errMess: action.payload};
+
+    case ActionTypes.ADD_COMMENTS:
+        var comment = action.payload;
+        return { ...state, comments: state.comments.concat(comment)};
+
+    default:
+      return state;
+  }
+};
